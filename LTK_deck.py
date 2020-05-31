f9bd7a6bfc0f211e44127d6e2f944820c39cd4f0
anyapriya <anyapriya@yahoo.com>
2020-05-31 11:10:34 -0400

    Adding in some basic cards to help get something up and running Modified plan in README

--- LTK_deck.py
+++ LTK_deck.py
@@ -33,8 +33,30 @@ class Deck:
 # cards will be represented numerically, so deck and hand just have ints that can be used to call and get info of card from this dict 
 # this dict might be replaced with a DB table, and just get the data from there and fill the info in with code
 # for now though, will hard code in a few cards just for use
-cards = {1: {"Card": "Strike", "Value": 10, "Suit": "Spades"},
-         2: {"Card": "Strike", "Value": 10, "Suit": "Spades"},
+cards = {1: {"Type": "Strike", "Value": 10, "Suit": "Spades"},
+         2: {"Type": "Strike", "Value": 10, "Suit": "Spades"},
+         3: {"Type": "Strike", "Value": 9, "Suit": "Spades"},
+         4: {"Type": "Strike", "Value": 9, "Suit": "Spades"},
+         5: {"Type": "Strike", "Value": 8, "Suit": "Spades"},
+         6: {"Type": "Strike", "Value": 8, "Suit": "Spades"},
+         7: {"Type": "Strike", "Value": 7, "Suit": "Spades"},
+         8: {"Type": "Strike", "Value": 10, "Suit": "Hearts"},
+         9: {"Type": "Strike", "Value": 10, "Suit": "Hearts"},
+
+
+
+         10: {"Type": "Peach", "Value": 3, "Suit": "Hearts"}, 
+         11: {"Type": "Peach", "Value": 4, "Suit": "Hearts"},
+         12: {"Type": "Peach", "Value": 6, "Suit": "Hearts"},
+
+
+         13: {"Type": "Dodge", "Value": 2, "Suit": "Diamonds"}, 
+         14: {"Type": "Dodge", "Value": 2, "Suit": "Diamonds"},
+         15: {"Type": "Dodge", "Value": 3, "Suit": "Diamonds"},
+         16: {"Type": "Dodge", "Value": 4, "Suit": "Diamonds"},
+         17: {"Type": "Dodge", "Value": 5, "Suit": "Diamonds"},
+         18: {"Type": "Dodge", "Value": 6, "Suit": "Diamonds"},
+         19: {"Type": "Dodge", "Value": 7, "Suit": "Diamonds"},
+
          }
-
-
+# Value will be an int so: 1 is Ace?  11 is Jack? 12 is Queen? 13 is King?  
--- README.md
+++ README.md
@@ -6,20 +6,23 @@ This is a tentative plan for getting the game up and running, but everything is 
 
 ## I. Get Game working!  
 
-#### A. Do basic outline to figure out structure
+#### <s>A. Do basic outline to figure out structure </s>
+
 #### B. Get something working 
-- no characters or equipment yet
-- a few cards hardcoded in
+- no characters or equipment yet, and a few cards hardcoded in
+- maybe first choose 3 cards like strike, dodge, peach and get game working for those
 
 #### C. Make unit tests now that something works 
 - use this to make sure it continues working as expected throughout process
 - make sure to keep adding unit tests as add more functionality
 
-#### D. Add in equipment functionality
+#### D. Add in non-equipment cards one at a time and make sure they each work (and write unit tests for each as well go) - this should be easy to divide up
 
-#### E. Set up database for cards and incorporate that into code (that way there can be multiple decks like base game, expansion, etc) - means can use SQL!
+#### E. Add in equipment functionality - might be a bit complicated
 
-#### F. Add in characters
+#### F. Set up database for cards and incorporate that into code (that way there can be multiple decks like base game, expansion, etc) - means can use SQL! Will be tedious.....
+
+#### G. Add in characters
 - probably the most tedious part of getting the game up and running
 - can start small with a few
 - can use database to keep track of which characters are allowed in which game mode (i.e. if some are Kingdom Wars only), but will still have to hardcode them all in
