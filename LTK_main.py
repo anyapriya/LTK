df01cdf9a67f894534912e4002dbd0da87a4319c
anyapriya <anyapriya@yahoo.com>
2020-06-03 09:14:37 -0400

    Changing main so work is done in a function (for testing) Updated README with more details for plan

--- LTK_main.py
+++ LTK_main.py
@@ -3,8 +3,9 @@ from LTK_Exceptions import PlayerCountOutOfBounds
 
 
 
-if __name__ == "__main__":
-    
+
+
+def getPlayers():
     players = input("Please enter how many players (4-10): ")
     while True:
         try:
@@ -17,7 +18,13 @@ if __name__ == "__main__":
             players = input("Did not enter a number.  Please input a number between 4 and 10 and hit enter: ")
         except PlayerCountOutOfBounds:
             players = input("Number was not an allowed number of players.  Please input a number between 4 and 10 and hit enter: ")
+    return players
 
+
+
+if __name__ == "__main__":
+    
+    players = getPlayers()
     board = LTK_board.board(players)
     # board.play()
 
